3586c52e5fbb15ca0d603a1eaa7be48fe1f26a82
manuzinhazxy <emanuelly.ramos.fonseca@escola.pr.gov.br>
2025-09-30 09:09:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const avanca = document.querySelectorA11('.btn-proximo');
 
 avanca.forEact(button => {
-    button.addEventListener
+    button.addEventListener('click', function()){
+        const atua = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    }
 })
